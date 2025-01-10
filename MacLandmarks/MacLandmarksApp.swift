dfbb0a171c5ddc09af5c8bdf178b6d3f4d6b3b45
kelly <kelly>
2025-01-10 15:13:41 +0800

    Create MacLandmarks

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by UCOM-08 on 2025/1/10.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacLandmarksTests/MacLandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksTests.swift
+//  MacLandmarksTests
+//
+//  Created by UCOM-08 on 2025/1/10.
+//
+
+import Testing
+@testable import MacLandmarks
+
+struct MacLandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
